cee940ea1070a0a20abb666d886f45944d640bee
tgy <xiaogao@tgy.com>
2025-04-11 22:26:53 +0800

    Initial Commit

---
+++ FFmpegTS2AudioTests/FFmpegTS2AudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  FFmpegTS2AudioTests.swift
+//  FFmpegTS2AudioTests
+//
+//  Created by tanggaoyuang on 2025/4/11.
+//
+
+import Testing
+@testable import FFmpegTS2Audio
+
+struct FFmpegTS2AudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
